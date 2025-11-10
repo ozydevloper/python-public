ea5d16cb56f0ca64e3a860ba4df5c02cb7f82d5c
ozydevloper <ozydeveloper@gmail.com>
2025-11-10 21:40:23 +0700

    bener ga

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class App:
         self.playwright = sync_playwright().start()
         self.browser = self.playwright.chromium.launch_persistent_context(user_data_dir="./../ICRMAUTO", headless=False, channel="chrome")
         self.page = self.browser.new_page()
+        self.page.on('response', self.handle_response)
         self.page.goto("https://icrmplus.iconpln.co.id/icrm/permohonan/corporate/disposition-to-partner", wait_until='networkidle')
 
         self.input_search = self.page.locator("//html/body/div/div[1]/div[1]/div/main/div/div[2]/div[3]/div/form/div/div/div[1]/div/div/div/div[2]/input")
@@ -16,11 +17,33 @@ class App:
         self.page.get_by_role('button', name="Nama Perusahaan", exact=True).click()
         self.page.get_by_role('menuitem', name="ID Permohonan", exact=True).click()
 
+        self.data = None
+        self.data_action = []
+
+    def handle_response(self, response):
+        if response.url == "https://rcrm.iconpln.co.id/icrm-be-backoffice-production/permohonan/korporat/disposisi/detail/lihat":
+            self.data = response.json()['data']['data']
+
+
     def step(self):
         self.input_search.clear()
         self.input_search.fill(ID_DUMMY)
         self.button_search.click()
-        self.page.wait_for_timeout(60000)
+
+        self.page.get_by_role('gridcell', name=ID_DUMMY, exact=True).click()
+        while True:
+            self.page.wait_for_timeout(100)
+            if self.data != None:
+                print("Data didapatkan")
+                for i in self.data:
+                    row = i
+                    if row['disposisiStatus'] == '0':
+                        self.data_action.append(row['idPermohonanProduk'])
+                break
+        
+        for idPermohonan in self.data_action:
+            self.page.get_by_role('gridcell', name=idPermohonan, exact=True).click()
+            self.page.wait_for_timeout(60000)
 
 
     def run(self):
