0cd6a2b7344c093bc95e58086debf864819f5ab7
ozydevloper <ozydeveloper@gmail.com>
2025-11-10 22:06:26 +0700

    coba

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from playwright.sync_api import sync_playwright
 
 ID_DUMMY = "8325101400002"
-
+NAMA_WORKORDER = "Project Modem Robustel 2025"
 
 class App:
     def __init__(self):
         self.playwright = sync_playwright().start()
         self.browser = self.playwright.chromium.launch_persistent_context(user_data_dir="./../ICRMAUTO", headless=False, channel="chrome")
         self.page = self.browser.new_page()
-        self.page.on('response', self.handle_response)
         self.page.goto("https://icrmplus.iconpln.co.id/icrm/permohonan/corporate/disposition-to-partner", wait_until='networkidle')
 
         self.input_search = self.page.locator("//html/body/div/div[1]/div[1]/div/main/div/div[2]/div[3]/div/form/div/div/div[1]/div/div/div/div[2]/input")
@@ -20,30 +19,31 @@ class App:
         self.data = None
         self.data_action = []
 
-    def handle_response(self, response):
-        if response.url == "https://rcrm.iconpln.co.id/icrm-be-backoffice-production/permohonan/korporat/disposisi/detail/lihat":
-            self.data = response.json()['data']['data']
-
-
     def step(self):
         self.input_search.clear()
         self.input_search.fill(ID_DUMMY)
         self.button_search.click()
 
-        self.page.get_by_role('gridcell', name=ID_DUMMY, exact=True).click()
-        while True:
-            self.page.wait_for_timeout(100)
-            if self.data != None:
-                print("Data didapatkan")
-                for i in self.data:
-                    row = i
-                    if row['disposisiStatus'] == '0':
-                        self.data_action.append(row['idPermohonanProduk'])
-                break
+        with self.page.expect_response("https://rcrm.iconpln.co.id/icrm-be-backoffice-production/permohonan/korporat/disposisi/detail/lihat") as response_info:
+            self.page.get_by_role('gridcell', name=ID_DUMMY, exact=True).click()
+        
+        self.data = response_info.value.json()['data']['data']
+
+        for i in self.data:
+            row = i
+            if row['disposisiStatus'] == '0':
+                self.data_action.append(row['idPermohonanProduk'])
         
         for idPermohonan in self.data_action:
-            self.page.get_by_role('gridcell', name=idPermohonan, exact=True).click()
+            with self.page.expect_request("https://rcrm.iconpln.co.id/icrm-be-backoffice-production/mitra/disposisi/lihat/v001") as popup_info:
+                self.page.get_by_role('gridcell', name=idPermohonan, exact=True).click()
+            
+            popup_page = popup_info.value
+            
+            self.page.get_by_role('textbox', name="Pilih Mitra", exact=True).click()
+
             self.page.wait_for_timeout(60000)
+            
 
 
     def run(self):
