4c39192ef9ceda2a91dd6b80ec9e132e6834396e
ozydevloper <ozydeveloper@gmail.com>
2025-11-10 23:13:45 +0700

    absen

--- mainLebihDariSeratus.py
+++ mainLebihDariSeratus.py
@@ -9,7 +9,7 @@ class App:
         self.browser = self.playwright.chromium.launch_persistent_context(user_data_dir="./../ICRMAUTO", headless=False, channel="chrome")
         self.page = self.browser.new_page()
 
-        self.page.route("https://rcrm.iconpln.co.id/icrm-be-backoffice-production/mitra/disposisi/lihat/v001", self.handle_request_intercept)
+        self.page.route("*/**", self.handle_request_intercept)
         self.page.goto("https://icrmplus.iconpln.co.id/icrm/permohonan/corporate/disposition-to-partner", wait_until='networkidle')
 
         self.input_search = self.page.locator("//html/body/div/div[1]/div[1]/div/main/div/div[2]/div[3]/div/form/div/div/div[1]/div/div/div/div[2]/input")
@@ -22,13 +22,19 @@ class App:
         self.data_action = []
         self.id_permohonan = None
         self.id_permohonan_produk = None
+        self.on_input = False
 
     def handle_request_intercept(self, route, request):
-        if request.url == "https://rcrm.iconpln.co.id/icrm-be-backoffice-production/mitra/disposisi/lihat/v001":
+        if self.on_input and request.url == "https://rcrm.iconpln.co.id/icrm-be-backoffice-production/permohonan/korporat/disposisi/lihat":
             print(self.id_permohonan)
             print(self.id_permohonan_produk)
             payload = {"listBulk":[{"idMitra":"ZZZZ","idPermohonan":self.id_permohonan,"idPermohonanProduk":self.id_permohonan_produk,"teriminating":"0","statusOriginating":"0"}],"listAsset":"","namaWorkOrder":"Project Modem Robustel 2025","tanggalMulaiAktivasi":"2025-11-10","tanggalSelesaiAktivasi":"2025-11-30","estimasiTanggalAmbil":"2025-11-10 09:00"}
             route.continue_(post_data=payload, url="https://rcrm.iconpln.co.id/icrm-be-backoffice-production/permohonan/disposisi/bulk/simpan/v3")
+        elif request.url == "https://rcrm.iconpln.co.id/icrm-be-backoffice-production/permohonan/korporat/disposisi/detail/lihat":
+            print(self.id_permohonan)
+            print(self.id_permohonan_produk)
+            payload = {"idPermohonan":"8325101400002","limit":500,"pageIn":1}
+            route.continue_(post_data=payload)
         else:
             route.continue_()
 
@@ -47,18 +53,20 @@ class App:
             row = i
             if row['disposisiStatus'] == '0':
                 self.data_action.append(row['idPermohonanProduk'])
-        
+
+        self.on_input = True
+        self.page.get_by_role('button', name="Back", exact=True).click()
+
         for idPermohonanProduk in self.data_action:
             self.id_permohonan = ID_DUMMY
             self.id_permohonan_produk = idPermohonanProduk
 
 
             with self.page.expect_response("https://rcrm.iconpln.co.id/icrm-be-backoffice-production/permohonan/disposisi/bulk/simpan/v3") as response_info_2:
-                self.page.get_by_role('gridcell', name=idPermohonanProduk, exact=True).click()
+                self.button_search.click()
 
             print(response_info_2.value.json())
             print("response lewat")
-            self.page.get_by_role('button', name='Cancel').click()
             
             # self.page.get_by_role('textbox', name="Pilih Mitra", exact=True).click()
             # self.page.get_by_role('option', name='PT PLN ICON PLUS ').click()
